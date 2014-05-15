c3263c1dc25e31211f1870a8dfdaf1d1a8954655
luis miguel ponce <miguelp@teqspring.com>
2014-05-15 04:11:51 +0200

    pdf

--- module/Application/src/Application/Permissions/AclListener.php
+++ module/Application/src/Application/Permissions/AclListener.php
@@ -25,7 +25,6 @@ class AclListener implements ListenerAggregateInterface{
     }
     
     public function onDispatch(MvcEvent $e){
-    	
         $acl = new Acl();
         
         $acl->addRole(new Role('O'))
