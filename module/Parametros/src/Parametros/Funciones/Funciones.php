500529628b6dfec76337d60c79a12dcfc62a1d66
Emanuel Carrasco <emanuelcarrasco@gmail.com>
2014-06-08 21:55:26 -0500

    Cambios Layout
    Change-Id: I1162990afe267e0ffd1d1294b12ee8b561c04b74

--- module/Parametros/src/Parametros/Funciones/Funciones.php
+++ module/Parametros/src/Parametros/Funciones/Funciones.php
@@ -30,6 +30,33 @@ class Funciones
             break;
         } 
 
-    }              
+    }        
+
+    public static function fechaFormateada($date,$language='E',$inFormat='d/m/Y'){
+    	$months=array(
+    			'I'=>array(
+    					'1'=>'january',     '2'=>'february',    '3'=>'march',
+    					'4'=>'april',       '5'=>'may',         '6'=>'june',
+    					'7'=>'july',        '8'=>'august',      '9'=>'september',
+    					'10'=>'october',    '11'=>'november',   '12'=>'december'
+    			),
+    			'E'=>array(
+    					'1'=>'enero',       '2'=>'febrero',     '3'=>'marzo',
+    					'4'=>'abril',       '5'=>'mayo',        '6'=>'junio',
+    					'7'=>'julio',       '8'=>'agosto',      '9'=>'septiembre',
+    					'10'=>'octubre',    '11'=>'noviembre',  '12'=>'diciembre'
+    			)
+    	);
+    	$date_info=date_parse_from_format($inFormat,$date);
+    	switch($language){
+    		case 'I':
+    			return $months[$language][$date_info['month']].' '.$date_info['day'].', '.$date_info['year'];
+    			break;
+    		case 'E':
+    			return $date_info['day'].' de '.$months[$language][$date_info['month']].' de '.$date_info['year'];
+    			break;
+    	}
+    
+    }
 
 }
